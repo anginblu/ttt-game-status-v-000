5f1823bb7e8ccb4dd472d03437962afa3ae908f0
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 21:56:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ def won?(board)
   if board.any? {|current_player| current_player == "X" || current_player == "O"}
     current = []
     WIN_COMBINATIONS.each do |win|
-      win.each do |i|
+      win.all do |i|
         if win.all?
         board(i) == "X"
         win
