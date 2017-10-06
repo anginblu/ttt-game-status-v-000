cbb91a0937b8139bf516a7f7ef30d5e0ce0f16c9
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 20:56:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
+win1 = [0, 1, 2]
+win2 = [3, 4, 5]
+win3 = [6, 7, 8]
