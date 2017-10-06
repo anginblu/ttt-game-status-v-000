5f49b79a23a1c9386e737013b7150a3adef8057d
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 22:46:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,8 +16,10 @@ WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
   board.each do |index|
-    if position_taken?(board, index) == false
-    someone_won
+    if position_taken?(board, index)
+      win1.all? |i|
+        board(i) == "X"
+          
     end
   end
 end
