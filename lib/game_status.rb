8b3ad4b8258f4f1747e1952211ca2626b15e0a3a
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 22:29:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,8 +15,6 @@ win8 = [6,4,2]
 WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
-  someone_won == false
-  current = []
   if board.any? {|current_player| current_player == "X" || current_player == "O"}
     true
   else
