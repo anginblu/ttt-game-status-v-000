945f46735cc3aac9ca3c144b0d7bc042e8cf8515
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 21:02:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ win8 = [6,4,2]
 WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
-  board.any do |win|
+  board.any do |draw|
     win.each do |win_combination|
 
 end
