1f5ba373919f55c427c9c82f9eb6bb4247e4ba4b
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 23:28:22 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -15,8 +15,8 @@ win8 = [6,4,2]
 WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
+  someone_won = false 
   WIN_COMBINATIONS.each do |win|
-    current = []
     someone_won = true if win.all? {|i| board[i] == "X"}
     someone_won = true if win.all? {|i| board[i] == "O"}
     someone_won = false if win.any? {|i| board[i] == " "}
