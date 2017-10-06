e69a80cefd36d1965ca6dbca50873c82209f7707
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 23:36:25 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -18,7 +18,7 @@ def won?(board)
   someone_won = false
   current = []
   WIN_COMBINATIONS.each do |win|
-      current << i if win.all? {|i| board[i] == "X"}
+    current << i if win.all? {|i| board[i] == "X"}
     end
     current
   end
