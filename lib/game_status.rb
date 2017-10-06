ab95e76d5a0c19ea96f54d23fa30a5c6d4de4f6f
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 21:59:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,15 +16,9 @@ WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
   if board.any? {|current_player| current_player == "X" || current_player == "O"}
-    current = []
+    Xwon == true
     WIN_COMBINATIONS.each do |win|
-      win.all do |i|
-        if board(i) == "X"
-        win
-        elsif board(i) == "O"
-        win
-        else
-          false
+      win.all do {|i| win if board(i) == "X"}
         end
       end
     end
