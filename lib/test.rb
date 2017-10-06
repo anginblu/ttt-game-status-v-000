46965ea8966f154a4c801306ec75dfe8b76433d1
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 22:52:58 +0000

    automatically backed up by learn

--- lib/test.rb
+++ lib/test.rb
@@ -8,3 +8,24 @@ WIN_COMBINATIONS.each do |win|
   someone_won
   current
 end
+
+
+def full?(board)
+  if board.all? {|draw| draw == "X" || draw == "O"}
+    true
+  else
+    false
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+end
+
