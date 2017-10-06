873a3a3c924f906a098ff2d50337f188b3a51bfb
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 23:27:14 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -19,7 +19,7 @@ def won?(board)
     current = []
     someone_won = true if win.all? {|i| board[i] == "X"}
     someone_won = true if win.all? {|i| board[i] == "O"}
-    someone_won = false if win.all? {|i| board[i] !== "O"}
+    someone_won = false if win.all? {|i| board[i] == " "}
   end
   someone_won
 end
