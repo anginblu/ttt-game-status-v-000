83173ae51a031beec32267694d2e39ab80d8df08
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:37:13 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -25,10 +25,10 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       current = [win_index_1, win_index_2, win_index_3]
-      puts current
+      current
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       current = [win_index_1, win_index_2, win_index_3]
-      puts current
+      current
     else
       puts false
     end
