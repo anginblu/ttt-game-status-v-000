539ccecc92d9c0ddcc4a8193f8202d2fffc6d858
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:10:19 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -14,7 +14,6 @@ win7 = [0,4,8]
 win8 = [6,4,2]
 WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
-
 def won?(board)
   WIN_COMBINATIONS.each do |win|
     win_index_1 = win[0]
@@ -25,8 +24,29 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       [win_index_1, win_index_2, win_index_3]
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        [win_index_1, win_index_2, win_index_3]
     else
       false
     end
   end
+end
+
+def full?(board)
+  if board.all? {|draw| draw == "X" || draw == "O"}
+    true
+  else
+    false
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
 end
