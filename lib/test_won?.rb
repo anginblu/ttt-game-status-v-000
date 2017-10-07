fa6ddb56c801031b8b8b20b9f2e6438507920777
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:42:36 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -26,8 +26,10 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       someone_won = true
+      current = [win_index_1, win_index_2, win_index_3]
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       someone_won = true
+      current = [win_index_1, win_index_2, win_index_3]
     else
       false
     end
