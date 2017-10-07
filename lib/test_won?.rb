cfd366a433401dd46742987e0aec59aaca34d44a
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:31:30 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -23,14 +23,14 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    if position_taken(board, win_index1) && position_taken(board, win_index2) && position_taken(board, win_index3)
+    if position_taken(board, win_index_1) && position_taken(board, win_index_2) && position_taken(board, win_index_3)
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
       current = [win_index_1, win_index_2, win_index_3]
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       current = [win_index_1, win_index_2, win_index_3]
       else
         false
-      end 
+      end
     else
       false
     end
