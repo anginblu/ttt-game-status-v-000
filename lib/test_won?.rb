5a2125a6419754f79360ba86ecc19a2083e3a998
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:22:13 +0000

    automatically backed up by learn

--- lib/test_won?.rb
+++ lib/test_won?.rb
@@ -15,6 +15,7 @@ win8 = [6,4,2]
 WIN_COMBINATIONS = [win1, win2, win3, win4, win5, win6, win7, win8]
 
 def won?(board)
+  current = []
   WIN_COMBINATIONS.each do |win|
     current = []
     win_index_1 = win[0]
