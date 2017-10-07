acc00a4d35e4c37ee2556b0c1c18531afe0378c2
Jacqueline Lee <anginblu@gmail.com>
2017-10-07 00:39:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,9 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       true
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      current = [win_index_1, win_index_2, win_index_3]
+      true
     else
-      current = []
+      false
     end
   if current =
   end
